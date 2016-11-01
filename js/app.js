e04e3944704e7a6fd73444205d8cceba285579fe
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-10-31 22:45:45 -0600

    Added and completed projects view.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-var app = angular.module("portfolio", ['ngRoute']);
+var app = angular.module("portfolio", ['ngRoute', 'ngAnimate']);
 app.config(function($routeProvider) {
     $routeProvider
     .when('/', {
       templateUrl: 'partials/landing.html',
       controller: 'MainController'
     })
-      .when('/home', {
-        templateUrl: 'partials/home.html',
-        controller: 'HomeController'
-      })
+      .when('/about', {
+        templateUrl: 'partials/about.html',
+        controller: 'AboutController'
+    })
+      .when('/projects', {
+        templateUrl: 'partials/projects.html',
+        controller: 'AboutController'
+    })
+      .when('/contact', {
+      templateUrl: 'partials/contact.html',
+      controller: 'AboutController'
+    })
+
 });
--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,13 @@
 app.controller('MainController', function($scope){
-  $scope.view{};
+  $scope.view = {};
 })
 
-app.controller('HomeController', function($scope){
-  $scope.view={}
+app.controller('AboutController', function($scope){
+  $scope.view = {};
+})
+app.controller('ProjectsController', function($scope){
+  $scope.view = {};
+})
+app.controller('ContactController', function($scope){
+  $scope.view = {};
 })
