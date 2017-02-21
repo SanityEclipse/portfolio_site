e905e40536e594acbfdcd80fc3091cda7f5dece1
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-02-21 01:21:19 -0700

    Tweaking hyper links to work properly.

--- app/app.routes.js
+++ app/app.routes.js
@@ -5,16 +5,17 @@
 
   config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
 
-  function config($stateProvider, $urlRouterProvider, $locationProvider){
+  function config($stateProvider, $urlRouterProvider, $locationProvider) {
 
     $locationProvider.html5Mode(true)
 
     $stateProvider
-    
+
       .state({ name: 'home', url: '/', component: 'siteLanding'})
       .state({ name: 'about', url: '/about-me', component: 'aboutMe'})
       .state({ name: 'projects', url: '/my-projects', component: 'myProjects'})
       .state({ name: 'contact', url: '/contact-info', component: 'contactInfo'})
+      .state({ name: 'gitHub', url: 'https://github.com/SanityEclipse', external: true})
 
   }
 
